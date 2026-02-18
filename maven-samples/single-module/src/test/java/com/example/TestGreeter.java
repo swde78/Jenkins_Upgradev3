cb13614e38f95574e825db2e0bc5e20785570568
swde78 <33987839+swde78@users.noreply.github.com>
2026-02-18 05:38:38 -0800

    Add failing test for internationally_Field method
    Added a test for the internationally_Field method that currently fails.

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -26,7 +26,11 @@ public class TestGreeter {
 
     assertThat(greeter.greet(someone), containsString(someone));
   }
-  
+
+  @Test
+  public void internationally_Field() {
+     assertTrue(false);
+  }
 
   @Test
   public void greetShouldIncludeGreetingPhrase() {
